91cf37615075ea4f7f8e44a2f1d5fd1e07039026
this-Aditya <adityamishra2217@gmail.com>
2026-03-03 19:57:06 +0000

    Added utility method for contracts

---
+++ contract/src/main/kotlin/org/radarbase/contract/utils/Env.kt
@@ -0,0 +1,9 @@
+package org.radarbase.contract.utils
+
+object Env {
+    const val PROJECT_SERVICE = "project-service"
+    const val USER_SERVICE = "user-service"
+    const val PARTICIPANT_SERVICE = "participant-service"
+    const val CONFIG_SERVICE = "config-service"
+    const val DATA_SOURCES_SERVICE = "data-sources-service"
+}
