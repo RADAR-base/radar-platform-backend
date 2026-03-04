a3f2ddad37d749b8e3a0b001d14a2868eeb1d94f
this-Aditya <adityamishra2217@gmail.com>
2026-03-04 12:29:30 +0000

    Correct license

--- contract/src/main/kotlin/org/radarbase/contract/service/DataSourcesServiceContract.kt
+++ contract/src/main/kotlin/org/radarbase/contract/service/DataSourcesServiceContract.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2025 King's College London
+ * Copyright 2026 King's College London
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
