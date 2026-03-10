188b35e8e2514cc743f12c3cacc6b3f8260ab3fc
this-Aditya <adityamishra2217@gmail.com>
2026-03-10 17:07:30 +0000

    Include builds dynamically and and add repositories in settings file

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,34 @@
-rootProject.name = "platform-backend"
+rootProject.name = "radar-platform-backend"
 
-include(":core")
-include(":contract")
-include(":delegate-api")
-include(":user-service")
-include(":data-sources-service")
-include(":data-service")
-include(":project-service")
-include(":config-service")
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    @Suppress("UnstableApiUsage")
+    repositories {
+        mavenCentral()
+        maven {
+            url = uri("https://oss.sonatype.org/content/repositories/snapshots")
+            mavenContent { snapshotsOnly() }
+        }
+        maven { url = uri("https://jitpack.io") }
+    }
+}
+
+rootDir.listFiles()
+    ?.also {
+        println("File is: $it")
+    }
+    ?.filter { it.isDirectory }
+    ?.filter { File(it, "build.gradle.kts").exists() || File(it, "build.gradle").exists() }
+    ?.forEach { dir ->
+        val modulePath = ":${dir.name}"
+        println("Adding module: $modulePath")
+        include(modulePath)
+        project(modulePath).projectDir = dir
+        println("Module added with directory ${dir.absolutePath}")
+    }
