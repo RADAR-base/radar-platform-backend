3b1621089d8501fb7c7ca31430ee08037e6c5a51
this-Aditya <adityamishra2217@gmail.com>
2026-03-12 18:03:20 +0000

    Update gradle and revert to manual module inclusion in settings script

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,28 +7,10 @@ pluginManagement {
     }
 }
 
-dependencyResolutionManagement {
-    @Suppress("UnstableApiUsage")
-    repositories {
-        mavenCentral()
-        maven {
-            url = uri("https://oss.sonatype.org/content/repositories/snapshots")
-            mavenContent { snapshotsOnly() }
-        }
-        maven { url = uri("https://jitpack.io") }
-    }
-}
-
-rootDir.listFiles()
-    ?.also {
-        println("File is: $it")
-    }
-    ?.filter { it.isDirectory }
-    ?.filter { File(it, "build.gradle.kts").exists() || File(it, "build.gradle").exists() }
-    ?.forEach { dir ->
-        val modulePath = ":${dir.name}"
-        println("Adding module: $modulePath")
-        include(modulePath)
-        project(modulePath).projectDir = dir
-        println("Module added with directory ${dir.absolutePath}")
-    }
+include(":config-service")
+include(":contract")
+include(":core")
+include(":data-sources-service")
+include(":delegate-api")
+include(":project-service")
+include(":user-service")
