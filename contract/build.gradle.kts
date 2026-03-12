532a45ca0f68a276aebc53171686d5a79ba9c1ae
this-Aditya <adityamishra2217@gmail.com>
2026-03-12 21:59:43 +0000

    Optimizations in core and contract library buildscripts

--- contract/build.gradle.kts
+++ contract/build.gradle.kts
@@ -1,18 +1,3 @@
-plugins {
-    id("org.jetbrains.kotlin.jvm")
-    id("org.jlleitschuh.gradle.ktlint")
-    kotlin("plugin.serialization")
-}
-
 dependencies {
-    api(project(":core"))
-    api("org.radarbase:radar-jersey:0.12.1")
-
-    implementation("io.ktor:ktor-client-core:2.3.5")
-    implementation("io.ktor:ktor-client-cio:2.3.5")
-    implementation("io.ktor:ktor-client-content-negotiation:2.3.5")
-    implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.5")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.2")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    "implementation"(project(":core"))
 }
-
--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,22 +1,4 @@
-plugins {
-    id("org.jetbrains.kotlin.jvm")
-    id("org.jlleitschuh.gradle.ktlint")
-    kotlin("plugin.serialization")
-}
-
 dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.2")
-
-    implementation("io.ktor:ktor-client-core:2.3.5")
-    implementation("io.ktor:ktor-client-cio:2.3.5")
-    implementation("io.ktor:ktor-client-content-negotiation:2.3.5")
-    implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.5")
-
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
-
-    implementation("jakarta.inject:jakarta.inject-api:2.0.1")
-    implementation("jakarta.ws.rs:jakarta.ws.rs-api:3.1.0")
-
-    implementation("org.slf4j:slf4j-api:2.0.9")
-    implementation("ch.qos.logback:logback-classic:1.4.14")
+    "implementation"(libs.jakarta.inject)
+    "implementation"(libs.jakarta.ws.rs)
 }
