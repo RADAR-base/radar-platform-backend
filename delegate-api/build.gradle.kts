a02ccb1de27feff49aed9e2a13402845a4616731
this-Aditya <adityamishra2217@gmail.com>
2026-03-18 12:56:42 +0000

    Remving the jar and shadowjar task configs in support of startScript

--- delegate-api/build.gradle.kts
+++ delegate-api/build.gradle.kts
@@ -11,19 +11,3 @@ dependencies {
     implementation(project(":core"))
     testImplementation(libs.mockk)
 }
-
-tasks.jar {
-    manifest {
-        attributes(mapOf("Main-Class" to application.mainClass.get()))
-    }
-}
-
-tasks.shadowJar {
-    archiveBaseName.set("app")
-    archiveVersion.set("")
-    archiveClassifier.set("")
-    mergeServiceFiles()
-    manifest {
-        attributes(mapOf("Main-Class" to application.mainClass.get()))
-    }
-}
